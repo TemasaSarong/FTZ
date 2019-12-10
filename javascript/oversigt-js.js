c3efda5e194603fc527f726f6db216082c21be4b
lgvillemoes <lgvillemoes@gmail.com>
2019-12-10 12:14:47 +0100

    Edited code after feedback

---
+++ javascript/oversigt-js.js
@@ -0,0 +1,14 @@
+//JavaScript code inspired by: 
+//https://stackoverflow.com/questions/36184252/javascript-only-li-text-slider
+
+
+var current = 0,
+	slides = document.getElementsByClassName("slideTextt");
+
+setInterval(function() {
+	for (var i = 0; i < slides.length; i++) {
+	slides[i].style.opacity = 0;
+	}
+	current = (current != slides.length - 1) ? current + 1 : 0;
+	slides[current].style.opacity = 1;
+}, 2000);
