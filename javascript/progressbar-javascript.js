a893fabce11806b79abf65a52ff41f68fea14bd1
Julie <julie@jbgrafik.dk>
2019-12-13 11:17:45 +0100

    progress bar

--- javascript/progressbar-javascript.js
+++ javascript/progressbar-javascript.js
@@ -1,9 +1,17 @@
+<<<<<<< HEAD
+=======
+// JavaScript Document
+>>>>>>> 907a264abf5cce5bc4f42fb79a1358f2d7ffcd32
 var i = 0;
 function move() {
   if (i == 0) {
     i = 1;
     var elem = document.getElementById("myBar");
+<<<<<<< HEAD
     var width = 10;
+=======
+    var width = 1;
+>>>>>>> 907a264abf5cce5bc4f42fb79a1358f2d7ffcd32
     var id = setInterval(frame, 10);
     function frame() {
       if (width >= 100) {
@@ -12,7 +20,10 @@ function move() {
       } else {
         width++;
         elem.style.width = width + "%";
+<<<<<<< HEAD
         elem.innerHTML = width  + "%";
+=======
+>>>>>>> 907a264abf5cce5bc4f42fb79a1358f2d7ffcd32
       }
     }
   }
