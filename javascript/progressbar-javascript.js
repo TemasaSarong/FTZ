06c26be545bf4e4cbcff55b81fe73a1ef4532508
Julie <julie@jbgrafik.dk>
2019-12-13 13:00:19 +0100

    rettelse

--- javascript/progressbar-javascript.js
+++ javascript/progressbar-javascript.js
@@ -1,17 +1,12 @@
-<<<<<<< HEAD
-=======
 // JavaScript Document
->>>>>>> 907a264abf5cce5bc4f42fb79a1358f2d7ffcd32
 var i = 0;
-function move() {
+function () {
   if (i == 0) {
     i = 1;
     var elem = document.getElementById("myBar");
-<<<<<<< HEAD
     var width = 10;
-=======
+
     var width = 1;
->>>>>>> 907a264abf5cce5bc4f42fb79a1358f2d7ffcd32
     var id = setInterval(frame, 10);
     function frame() {
       if (width >= 100) {
@@ -20,10 +15,7 @@ function move() {
       } else {
         width++;
         elem.style.width = width + "%";
-<<<<<<< HEAD
         elem.innerHTML = width  + "%";
-=======
->>>>>>> 907a264abf5cce5bc4f42fb79a1358f2d7ffcd32
       }
     }
   }
