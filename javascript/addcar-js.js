2e47096d72d0018603091d47c7036bacb6ae51c7
tobjen <54642280+tobjen@users.noreply.github.com>
2019-12-15 20:09:58 +0100

    Ændret i javakoden så den passer til nyt html id

--- javascript/addcar-js.js
+++ javascript/addcar-js.js
@@ -1,13 +1,18 @@
 // JavaScript Document
 function addcars(){
-			var data = ["Volkswagen Golf VI","1,4 TSI 122 Highline Variant DSG 5d", "Modelår: 2012", "166.551 km, 122 hk / 200 Nm"];
-			var string_value = document.getElementById('test').value;
-			if (string_value.length <= 4) {
-					document.getElementById("error").innerHTML = "Prøv at tilføje flere cifre";
-					document.getElementById("info").innerHTML = "";
-					}
-					else {
-					document.getElementById("info").innerHTML = data;
-					document.getElementById("error").innerHTML = "";
-					}
-					}
+	
+	var data = ["Volkswagen Golf VI","1,4 TSI 122 Highline Variant DSG 5d", "Modelår: 2012", "166.551 km, 122 hk / 200 Nm"];
+	var string_value = document.getElementById('Nummerplade').value;
+	
+		if (string_value.length <= 4) {
+			
+			document.getElementById("error").innerHTML = "Prøv at tilføje flere cifre";
+			document.getElementById("info").innerHTML = "";
+			}
+	
+		else {
+			
+			document.getElementById("info").innerHTML = data;
+			document.getElementById("error").innerHTML = "";
+			}
+			}
