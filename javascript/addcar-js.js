1a4b780d00dc7c783353cbf11e8295e656d0f2c7
tobjen <54642280+tobjen@users.noreply.github.com>
2019-12-15 19:40:05 +0100

    Opdateret java script till addcar, tilføjet fejlmeddelelse i html og stylet den i css

--- javascript/addcar-js.js
+++ javascript/addcar-js.js
@@ -1,10 +1,13 @@
 // JavaScript Document
 function addcars(){
 			var data = ["Volkswagen Golf VI","1,4 TSI 122 Highline Variant DSG 5d", "Modelår: 2012", "166.551 km, 122 hk / 200 Nm"];
-			if (document.getElementById("test").innerHTML != null) {
-					document.getElementById("info").innerHTML = data;
+			var string_value = document.getElementById('test').value;
+			if (string_value.length <= 4) {
+					document.getElementById("error").innerHTML = "Prøv at tilføje flere cifre";
+					document.getElementById("info").innerHTML = "";
 					}
 					else {
-					alert ("That doesn't seem right");
+					document.getElementById("info").innerHTML = data;
+					document.getElementById("error").innerHTML = "";
 					}
 					}
